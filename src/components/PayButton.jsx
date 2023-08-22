6b1ec9674c8df9b7c83d37e4d7f8d2398f80a37f
Cyrus-0101 <cyrus_goutlook.com>
2023-08-23 02:04:30 +0300

    Added our PayButton and refactored our Course and Courses components

---
+++ src/components/PayButton.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import propTypes from 'prop-types';
+
+import { url } from "../redux/api/apiSlice";
+import { selectCurrentToken } from "../redux/features/auth/authSlice";
+
+const PayButton = ({ cartItems }) => {
+  const isLoggedIn = useSelector((state) => selectCurrentToken(state));
+  const handleCheckout = () => {
+    axios
+      .post(`${url}/stripe/checkout-session`, {
+        cartItems,
+        isLoggedIn,
+      })
+      .then((response) => {
+        if (response.data.url) {
+          window.location.href = response.data.url;
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  return (
+    <>
+      <button onClick={() => handleCheckout()}>Check out</button>
+    </>
+  )
+}
+
+PayButton.propTypes = {
+  cartItems: propTypes.array.isRequired
+}
+
+export default PayButton
