5737c7d83890cb2ab63cebcc458479fd3740faff
Cyrus-0101 <cyrus_goutlook.com>
2023-08-22 21:23:48 +0300

    Added Forgot Password dummy page

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,36 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import { useForm } from "react-hook-form";
+
+const ForgotPassword = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    // the data object contains the user input information which will be used for validation and register
+    console.log(data);
+  };
+  return (
+    <div className="forgot_password">
+      <Header />
+      <form onSubmit={handleSubmit(onSubmit)} className="register-form">
+        <h3 className="sign-up__form--title center">Forgot Password</h3>
+        <div className="form__group">
+          <label htmlFor="mail" className="form__group--label">
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="form__group--input"
+            placeholder="your email address"
+            {...register("email")}
+          />
+        </div>
+        <button className="form__button">Reset Password</button>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+export default ForgotPassword;
