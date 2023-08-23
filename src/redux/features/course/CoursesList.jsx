30a4cd8c5f29243be0f2c8206c82edbb9a3965d1
Cyrus-0101 <cyrus_goutlook.com>
2023-08-23 12:40:59 +0300

    Added our Title for course area

--- src/redux/features/course/CoursesList.jsx
+++ src/redux/features/course/CoursesList.jsx
@@ -3,6 +3,7 @@ import { useGetCoursesQuery } from "../../features/course/courseApiSlice";
 import Loader from '../../../components/Loader';
 import { ToastNotification, showErrorToast } from '../../../components/Toast';
 import Course from './Course';
+import Title from "../../../components/Title";
 
 const CoursesList = () => {
 
@@ -26,6 +27,10 @@ const CoursesList = () => {
     content = (
       <>
         <section className="courses" id="courses">
+          <Title
+            subTitle="we have a variety of courses to make you drive your dream car"
+            mainTitle="browse our courses"
+          />
           {ids.map((id) => (<Course key={id} courseId={id} />))}
         </section>
       </>
