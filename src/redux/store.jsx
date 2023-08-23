42461b783bc3cbad564a639bb411fdef124861bb
Cyrus-0101 <cyrus_goutlook.com>
2023-08-23 21:12:00 +0300

    Aesthetics refactor

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,18 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./api/apiSlice";
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import authReducer from "./features/auth/authSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './api/apiSlice';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import authReducer from './features/auth/authSlice';
 import cartReducer from "./features/cart/cartSlice";
 
 export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer,
-    cart: cartReducer,
-
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
-});
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer,
+        cart: cartReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+})
 
 setupListeners(store.dispatch);
