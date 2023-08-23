928a9baf9e4850c280357d3927e49a540cfa6465
Cyrus-0101 <cyrus_goutlook.com>
2023-08-23 21:10:59 +0300

    Refactored Persist Login

--- src/redux/features/auth/PersistLogin.jsx
+++ src/redux/features/auth/PersistLogin.jsx
@@ -1,83 +1,50 @@
-import { Outlet, useNavigate } from "react-router-dom"
+/* eslint-disable no-undef */
+import { Link, Outlet, useNavigate } from "react-router-dom"
 import { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { useRefreshMutation } from "./authApiSlice"
 import { selectCurrentToken } from "./authSlice"
 
-import Loader from "../../../components/Loader"
-import UnauthorizedPage from "../../../components/401Page"
-
 import usePersist from "../../../hooks/usePersist"
 
 const PersistLogin = () => {
+    const [persist] = usePersist()
+    const token = useSelector(selectCurrentToken)
+    const effectRan = useRef(false)
+    const [trueSuccess, setTrueSuccess] = useState(false)
+    const [refresh, { isUninitialized, isSuccess, isError, error }] = useRefreshMutation()
 
-  const [persist] = usePersist()
+    useEffect(() => {
+        if (effectRan.current === true || process.env.NODE_ENV !== 'development') {
+            const verifyRefreshToken = async () => {
+                try {
+                    await refresh()
+                    setTrueSuccess(true)
+                } catch (err) {
+                    console.error(err)
+                }
+            }
 
-  const token = useSelector(selectCurrentToken)
-
-  const effectRan = useRef(false)
-
-  const navigate = useNavigate();
-
-  const [trueSuccess, setTrueSuccess] = useState(false)
-
-  const [refresh, {
-    isUninitialized,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  }] = useRefreshMutation()
-
-  useEffect(() => {
-
-    if (effectRan.current === true) { // React 18 Strict Mode
-
-      const verifyRefreshToken = async () => {
-        try {
-          await refresh()
-
-          setTrueSuccess(true)
+            if (!token && persist) verifyRefreshToken()
         }
-        catch (err) {
-          console.error(err)
-        }
-      }
+        return () => (effectRan.current = true)
+    }, [persist, refresh, token])
 
-      if (!token && persist) verifyRefreshToken()
+    if (!persist) {
+        return <Outlet />
+    } else if (isError) {
+        return (
+            <div className="my-4 text-xs text-red-500 text-center tracking-wide">
+                {`${error?.data?.message} - `}
+                <Link to="/login">Please login again</Link>
+            </div>
+        )
+    } else if (isSuccess && trueSuccess) {
+        return <Outlet />
+    } else if (token && isUninitialized) {
+        return <Outlet />
     }
-    return () => effectRan.current = true
-
-  }, [navigate, persist, refresh, token])
-
-  let content
-
-  if (!persist) { // persist: no
-    // Content is equal to OutLet but if not token redirect to login
-    if (!token) {
-      // Redirect to login approriately without using Navigate/ force page to be rendered is login page
-      window.location.href = '/login'
-
-    }
-    content = <Outlet />
-
-  } else if (isLoading) { // persist: yes, token: no
-    content = <Loader />
-
-  } else if (isError) { //persist: yes, token: no
-    content = (
-      <div className="mt-5 container">
-        <UnauthorizedPage message={error.data?.message} />
-      </div>
-    )
-  } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
-    content = <Outlet />
-
-  } else if (token && isUninitialized) { //persist: yes, token: yes
-    content = <Outlet />
-  }
-
-  return content
 }
+
 export default PersistLogin
