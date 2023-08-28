2082d4883e7b11ce0e1e73b02234ab3266087813
Cyrus-0101 <cyrus_goutlook.com>
2023-08-28 14:54:45 +0300

    Created our 400 Page

---
+++ src/components/400Page.jsx
@@ -0,0 +1,13 @@
+
+const BadRequestPage = () => {
+  return (
+    <div className="bad-request-container">
+      <h1 className="bad-request-title">400 - Bad Request</h1>
+      <p className="bad-request-message">
+        Oops! It seems there was a bad request. Please try again later.
+      </p>
+    </div>
+  );
+};
+
+export default BadRequestPage;
