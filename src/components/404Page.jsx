08573e97cd82ec0dde16809635f77ce2e0b6d7a3
Cyrus-0101 <cyrus_goutlook.com>
2023-08-28 14:55:34 +0300

    Created our 404 Page

--- src/components/404Page.jsx
+++ src/components/404Page.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <div className="not-found-container">
+      <div className="animated-text">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-message">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <Link to="/" className="home-button">
+          Go to Home Page
+        </Link>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
