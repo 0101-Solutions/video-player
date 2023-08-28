d5397ade836727c97a3f137de9d462682e9409e4
Cyrus-0101 <cyrus_goutlook.com>
2023-08-28 15:16:18 +0300

    Added fetch my orders to our preflight request

--- src/redux/features/auth/Prefetch.jsx
+++ src/redux/features/auth/Prefetch.jsx
@@ -9,11 +9,13 @@ const Prefetch = () => {
   useEffect(() => {
     // Manually subscribing to the endpoints
     const courses = store.dispatch(coursesApiSlice.endpoints.getCourses.initiate());
+    const myOrders = store.dispatch(coursesApiSlice.endpoints.getMyCourses.initiate());
 
 
     return () => {
       // Manually unsubscribing from the endpoints. Clean up
       courses.unsubscribe();
+      myOrders.unsubscribe();
     }
   }, [])
 
