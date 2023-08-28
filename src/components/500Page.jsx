cdb4e01704f002407163fd44567015859c5fc5b4
Cyrus-0101 <cyrus_goutlook.com>
2023-08-28 14:55:56 +0300

    Created our 500 Server Error Page

--- src/components/500Page.jsx
+++ src/components/500Page.jsx
@@ -1,8 +1,39 @@
+import { useEffect } from "react";
 
-const UnexpectedServerError = () => {
+const ServerErrorPage = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      document.body.classList.remove("loading");
+    }, 1000);
+  }, []);
+
   return (
-    <div>UnexpectedServerError</div>
-  )
-}
+    <div className="server-error-container">
+      <div className="animated-text">
+        <h1 className="server-error-title">500</h1>
+        <p className="server-error-message">
+          Oops! Something went wrong. Please try again later.
+        </p>
+      </div>
+      <div className="gears">
+        <div className="gear one">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className="gear two">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className="gear three">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UnexpectedServerError
+export default ServerErrorPage;
