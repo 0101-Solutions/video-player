932e4f13f95adba0342df29aeb0e4d5e95cbec5c
Cyrus-0101 <cyrus_goutlook.com>
2023-08-28 15:13:29 +0300

    Refactored our Hero section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,15 +3,14 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Footer from "../components/Footer";
 
-import CoursesList from "../redux/features/course/CoursesList";
-
 const Homepage = () => {
+  const description = `Entry-level driver training (ELDT) is required for drivers before obtaining a commercial driver's license (CDL).Prior to February 7, 2022, each state had its own commercial driver's license training requirements. To establish a single, national standard for obtaining your CDL, the Federal Motor Carrier Safety Association (FMCSA) has updated ELDT requirements. ELDT includes theory and behind-the-wheel training and must be completed with an FMCSA registered training program or provider–like TDI!`;
+
   return (
     <>
       <Header />
-      <Hero />
+      <Hero description={description} subTitle="What is ELDT?" mainTitle="CDL City Driving School" />
       <About />
-      <CoursesList />
       <Footer />
     </>
   )
