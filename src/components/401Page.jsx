969c0a65b561bddd434a57cc8fb128d743e10b07
Cyrus-0101 <cyrus_goutlook.com>
2023-08-28 14:54:58 +0300

    Created our 401 Page

--- src/components/401Page.jsx
+++ src/components/401Page.jsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
 
 const UnauthorizedPage = () => {
   return (
-    <div>401Page</div>
-  )
-}
+    <div className="unauthorized-container">
+      <div className="emoji-container">
+        <div className="emoji">🚫</div>
+        <div className="emoji">🚫</div>
+        <div className="emoji">🚫</div>
+        <div className="emoji">🚫</div>
+      </div>
+      <h1 className="unauthorized-title">Access Denied</h1>
+      <p className="unauthorized-message">
+        You do not have permission to view this site.
+      </p>
+      <Link to="/login" className="unauthorized-message">
+        Try Logging In Again To Access This Page
+      </Link>
+      <p className="error-code">Error Code: 401 Unauthorized</p>
+    </div>
+  );
+};
 
-export default UnauthorizedPage
+export default UnauthorizedPage;
