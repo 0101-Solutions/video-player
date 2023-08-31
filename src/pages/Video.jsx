bfc76ddef57ab6f44d42664ec6967a93d12722d7
Cyrus-0101 <cyrus_goutlook.com>
2023-08-31 16:05:31 +0300

    Refactored our VideoPlayer to fetch course info from state

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -2,19 +2,20 @@
 import { useEffect, useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { showSuccessToast } from '../components/Toast';
-import { useCompleteCourseMutation, useGetMyCoursesQuery } from '../redux/features/course/courseApiSlice';
-import Header from '../components/Header';
+import { selectCourseById, useCompleteCourseMutation, useGetMyCoursesQuery } from '../redux/features/course/courseApiSlice';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const VideoPlayerFn = () => {
-  const playerRef = useRef(null);
 
-  const {
-    data: courses,
-    isLoading: isLoadingCourses,
-    isSuccess: isSuccessCourses,
-    isError: isErrorCourses,
-    error: errorCourses
-  } = useGetMyCoursesQuery();
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  // Fetch the course from the state
+  const course = useSelector((state) => selectCourseById(state, id));
+
+  const playerRef = useRef(null);
 
   const [completeCourse, {
     isLoading,
@@ -22,17 +23,6 @@ const VideoPlayerFn = () => {
     isError,
     error
   }] = useCompleteCourseMutation();
-
-  // Map through the courses.data object and get the videos from the array.data in an array
-  function getVideoUrls(courses) {
-    const videos = courses?.data?.map((course) => course).map((video) => video.course.videos);
-
-    return videos
-  }
-
-  const previewUrl = courses?.data?.map((course) => course).map((video) => video.course.previewUrl);
-
-  const videos = getVideoUrls(courses).flat();
 
   const [activeVideoIndex, setActiveVideoIndex] = useState(0);
 
@@ -51,15 +41,17 @@ const VideoPlayerFn = () => {
   const handleVideoEnded = () => {
     const nextIndex = activeVideoIndex + 1;
 
-    if (nextIndex >= videos.length) {
+    if (nextIndex >= course.videos.length) {
       // Redirect to the first video when reaching the end
       localStorage.removeItem('activeVideoIndex');
       showSuccessToast(`You have completed the course successfully`)
       // Send API to update student to graduatedStudents 
 
-      window.location.reload();
+      navigate("/dashboard/completed-course")
+
+      // window.location.reload();
     } else {
-      videos.map((video) => {
+      course.videos.map((video) => {
         showSuccessToast(`Video ${video.title} watched successfully`)
 
         return { ...video, watched: true };
@@ -75,7 +67,7 @@ const VideoPlayerFn = () => {
       <div className="container">
         <div className="main-video-container">
           <ReactPlayer
-            url={videos[activeVideoIndex].url}
+            url={course.videos[activeVideoIndex].url}
             ref={playerRef}
             controls
             autoPlay
@@ -83,17 +75,17 @@ const VideoPlayerFn = () => {
             className="main-video"
             onEnded={handleVideoEnded}
           />
-          <h3 className="main-vid-title">Currently Watching: {videos[activeVideoIndex].title}</h3>
+          <h3 className="main-vid-title">Currently Watching: {course.videos[activeVideoIndex].title}</h3>
         </div>
 
         <div className="video-list-container">
-          {videos.map((video, index) => (
+          {course.videos.map((video, index) => (
             <div
               key={index}
               className={`list ${activeVideoIndex === index ? 'active' : ''}`}
               onClick={() => handleVideoClick(index)}
             >
-              <img src={previewUrl} className="list-video" />
+              <img src={course.previewUrl} className="list-video" />
               <h3 className="list-title">{video.title} {video.watched ? '✔️ Watched' : ''}</h3>
             </div>
           ))}
