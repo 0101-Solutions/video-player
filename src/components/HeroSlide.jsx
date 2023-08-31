15732ec8bb2144bcfde67cfc572c9e5c180e5e7f
Cyrus-0101 <cyrus_goutlook.com>
2023-08-31 12:44:04 +0300

    Made our Hero Page Dynamic

--- src/components/HeroSlide.jsx
+++ src/components/HeroSlide.jsx
@@ -1,10 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 import img from "../assets/Images/img-3.jpg";
 import img2 from "../assets/Images/img-2.jpg";
 import img3 from "../assets/Images/img-1.jpg";
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { selectCurrentToken } from "../redux/features/auth/authSlice";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const HeroSlide = ({ subTitle, mainTitle, description }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const isLoggedIn = useSelector((state) => selectCurrentToken(state))
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setIsAuthenticated(true)
+    }
+
+    if (!isLoggedIn) {
+      redirect('/login')
+      setIsAuthenticated(false)
+    }
+  }, [isLoggedIn]);
+
   return (
     <section className="hero-section">
       <div className="hero-section__textbox">
@@ -12,9 +31,11 @@ const HeroSlide = ({ subTitle, mainTitle, description }) => {
         <h2 className="hero-section__textbox--main-title">{mainTitle}</h2>
         <p className="hero-section__textbox--description">{description}</p>
         {/* <Link to="/">register</Link> */}
-        <Link to="/signup" className="hero-section__textbox--link">
-          register
-        </Link>
+        {!isAuthenticated
+          ? <Link to="/register" className="hero-section__textbox--link">register</Link>
+          : <Link to="/video-courses" className="hero-section__textbox--link">View Courses</Link>
+        }
+
       </div>
       <div className="hero__images">
         <img
