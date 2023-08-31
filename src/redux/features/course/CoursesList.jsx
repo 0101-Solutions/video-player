b52e1e8c78108aeeb65082e5f6f421e2d64df707
Cyrus-0101 <cyrus_goutlook.com>
2023-08-31 16:07:41 +0300

    Removed unused imports

--- src/redux/features/course/CoursesList.jsx
+++ src/redux/features/course/CoursesList.jsx
@@ -4,9 +4,6 @@ import Loader from '../../../components/Loader';
 import { ToastNotification, showErrorToast } from '../../../components/Toast';
 import Course from './Course';
 import Title from "../../../components/Title";
-import Header from "../../../components/Header";
-import Footer from "../../../components/Footer";
-// import { useGetMyOrdersQuery } from "../order/orderApiSlice";
 
 const CoursesList = () => {
 
@@ -29,15 +26,13 @@ const CoursesList = () => {
 
     content = (
       <>
-        <Header />
         <section className="courses" id="courses">
           <Title
             subTitle="we have a variety of courses to make you drive your dream car"
             mainTitle="browse our courses"
           />
-          {ids.map((id) => (<Course key={id} courseId={id} />))}
+          {ids.map((id) => <Course key={id} courseId={id} />)}
         </section>
-        <Footer />
       </>
     );
   }
