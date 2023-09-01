c8a17b212c8fe4edb8ed05e171b6cdd1364ab923
Cyrus-0101 <cyrus_goutlook.com>
2023-09-01 12:15:06 +0300

    Created Complete Course Page

--- src/pages/CompleteCoursePage.jsx
+++ src/pages/CompleteCoursePage.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
+import ConfettiExplosion from "react-confetti-explosion";
 
 const CompleteCoursePage = () => {
   return (
-    <div>CompleteCoursePage</div>
+    <div className="payment-successful" style={{ "height": "43.2vh" }}>
+      <h2>Course Completed Successfully</h2>
+      <ConfettiExplosion style={{ "left": "50%", "translateX": "-50%" }} force={0.8} duration={5000} particleCount={1000} width={2000} />
+      <p>
+        You will receive your certificate in your email. You can also view your certificate
+        {" "} <Link to="/dashboard/completed-course">by clicking here</Link>.
+      </p>
+    </div>
   )
 }
 
