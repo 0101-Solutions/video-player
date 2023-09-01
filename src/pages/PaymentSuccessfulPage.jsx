d45ac89156c772b7c200fcd5f5ad7d24ac2affec
Cyrus-0101 <cyrus_goutlook.com>
2023-09-01 12:16:23 +0300

    Added ConfettiExplosion and refactor

--- src/pages/PaymentSuccessfulPage.jsx
+++ src/pages/PaymentSuccessfulPage.jsx
@@ -1,32 +1,24 @@
 import { Link } from "react-router-dom";
 
-import { ToastNotification, showSuccessToast } from "../components/Toast";
 import { clearCart } from "../redux/features/cart/cartSlice";
-// import Footer from "../components/Footer";
+import ConfettiExplosion from "react-confetti-explosion";
 
 const PaymentSuccessful = () => {
-  // You can customize the success message
-  const successMessage = "Payment Successful! Thank you for your purchase.";
-
-  // Trigger the success toast
-  showSuccessToast(successMessage);
-
-  // Redirect after staying on page for 5 seconds
   setTimeout(() => {
     clearCart();
     window.location.href = "/dashboard/eldt-courses";
   }, 5000);
 
   return (
-    <div className="payment-successful">
+    <div className="payment-successful" style={{ "height": "60vh" }}>
       <div className="payment-successful-container">
         <h2>Payment Successful</h2>
+        <ConfettiExplosion force={0.8} duration={5000} particleCount={500} width={1600} />
         <p>
           You will shortly be redirected to the video courses page. You can also access your purchased course video(s)
           {" "} <Link to="/video-courses">by clicking here</Link>.
         </p>
       </div>
-      <ToastNotification />
     </div>
   );
 };
