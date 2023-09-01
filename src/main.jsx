0a713f9e1c35c592ebe3bc2a1c3d19425fa471f2
Cyrus-0101 <cyrus_goutlook.com>
2023-09-01 11:26:16 +0300

    Added Cookie Component and ToastNotification

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,31 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import CookieConsentComponent from './components/CookieConsent'
+import { ToastNotification } from './components/Toast'
+
+const acceptCookies = () => {
+  document.cookie = "cookieConsent=true; max-age=31536000; path=/";
+};
+
+// Check if cookieConsent is set to true
+// If so we can hide the cookie consent banner
+const cookieConsent = document.cookie.split(';').some((item) => item.trim().startsWith('cookieConsent='));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <Provider store={store}>
         <BrowserRouter>
+          {cookieConsent ? null : <CookieConsentComponent
+            cookieName="cookieConsent"
+            location="none"
+            buttonText="Accept Cookies"
+            overlay
+            overlayClasses="overlayclass"
+            handleCookieAccept={acceptCookies}
+          />}
+          <ToastNotification />
           <Routes>
             <Route path="/*" element={<App />} />
           </Routes>
