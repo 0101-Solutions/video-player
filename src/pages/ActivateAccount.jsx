676d93c03c19e1ab2b405f159b4529816f15691f
Cyrus-0101 <cyrus_goutlook.com>
2023-09-04 17:28:59 +0300

    Finished activate account page

--- src/pages/ActivateAccount.jsx
+++ src/pages/ActivateAccount.jsx
@@ -1,7 +1,12 @@
 
 const ActivateAccount = () => {
   return (
-    <div>ActivateAccount</div>
+    <div className="payment-successful" style={{ "height": "43.2vh" }}>
+      <h2>Verify Your Email</h2>
+      <p>
+        Welcome to CDL City Driving School. Check your email a link has been sent to you to verify your account.
+      </p>
+    </div>
   )
 }
 
