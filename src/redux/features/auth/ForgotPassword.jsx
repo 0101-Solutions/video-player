6adb81ef3662975091cc8a34496c4c4ece2706f5
Cyrus-0101 <cyrus_goutlook.com>
2023-09-04 15:13:17 +0300

    Finalized ForgotPassword page

--- src/redux/features/auth/ForgotPassword.jsx
+++ src/redux/features/auth/ForgotPassword.jsx
@@ -1,16 +1,35 @@
 import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useForgotPasswordMutation } from "./authApiSlice";
+import { showErrorToast, showSuccessToast } from "../../../components/Toast";
 
 const ForgotPassword = () => {
+  const navigate = useNavigate();
+
+  const [forgotPassword, { isSuccess, isError, error }] = useForgotPasswordMutation();
+
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = () => {
-    // the data object contains the user input information which will be used for validation and register
-    // console.log(data);
+  const onSubmit = (data) => {
+    forgotPassword(data.email);
   };
 
-  return (
-    <div className="forgot_password">
-      <form onSubmit={handleSubmit(onSubmit)} className="register-form">
+  let content;
+
+  console.log(isSuccess)
+
+  if (isSuccess) {
+    showSuccessToast("Password reset link sent to your email address");
+    navigate("/login")
+  }
+
+  if (isError) {
+    showErrorToast(error.data.message);
+  }
+
+  content = (
+    <div className="login">
+      <form onSubmit={handleSubmit(onSubmit)} className="login-form">
         <h3 className="sign-up__form--title center">Forgot Password</h3>
         <div className="form__group">
           <label htmlFor="mail" className="form__group--label">
@@ -20,13 +39,18 @@ const ForgotPassword = () => {
             type="email"
             id="email"
             className="form__group--input"
-            placeholder="your email address"
+            placeholder="Your registered email address"
             {...register("email")}
           />
         </div>
         <button className="form__button">Reset Password</button>
+        <p className="text-right" style={{ "marginTop": "-3rem" }}>
+          <Link to="/login">Existing User?</Link>
+        </p>
       </form>
     </div>
   );
+
+  return content;
 };
 export default ForgotPassword;
