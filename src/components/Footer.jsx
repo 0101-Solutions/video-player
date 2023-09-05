db4b8d5d4c29034dd8b151e62014a116883614d0
Cyrus-0101 <cyrus_goutlook.com>
2023-09-05 17:03:20 +0300

    Fixed content in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const date = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <div className="footer__top">
-        <p className="footer__logo-text">CDL City Driving</p>
+        <p className="footer__logo-text">ELDT Training Center</p>
 
       </div>
 
@@ -13,13 +15,13 @@ const Footer = () => {
           <p className="footer__links-title">Contacts</p>
           <ol className="footer__nav-list">
             <li className="footer__nav-item">
-              <Link>Application via this website</Link>{" "}
+              <Link>2700 Zanker Rd</Link>{" "}
             </li>
             <li className="footer__nav-item">
-              <Link>Call Us: +414-897654</Link>{" "}
+              <Link>San Jose California 95134</Link>{" "}
             </li>
             <li className="footer__nav-item">
-              <Link>email: cdlcity@drivingschool.com</Link>{" "}
+              <Link>email: <p className="text-lowercase">info@eldttraining.com</p></Link>{" "}
             </li>
           </ol>
         </div>
@@ -32,15 +34,15 @@ const Footer = () => {
             </li>
             <li className="footer__nav-item">
               {" "}
-              <Link to="/eldt-courses">ELDT Courses</Link>{" "}
+              <Link to="/eldt-courses">ELDT Course Certificate</Link>{" "}
             </li>
           </ol>
         </div>
+      </div><div className="footer__copyright">
+        <p>The moment you complete the course we report the completion to the Federal Registry. You can then begin the behind-the-wheel training.</p>
+        <p>&copy; {date} ELDT Training. All rights reserved</p>
       </div>
-      <p className="footer__copyright">
-        &copy; 2023 CDL City Driving School. All rights reserved
-      </p>
-    </footer>
+    </footer >
   );
 };
 
